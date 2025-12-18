28c31067539967e7ebfa7b481b999e58ee1a8180
kundan-thakur61 <152484837+kundan-thakur61@users.noreply.github.com>
2025-12-18 15:25:44 +0530

    Delete frontend directory

--- frontend/src/api/wishlistAPI.js
+++
@@ -1,9 +0,0 @@
-import axiosClient from './axiosClient';
-
-const wishlistAPI = {
-  getWishlist: () => axiosClient.get('/wishlist'),
-  addToWishlist: (productId) => axiosClient.post('/wishlist', { productId }),
-  removeFromWishlist: (productId) => axiosClient.delete(`/wishlist/${productId}`),
-};
-
-export default wishlistAPI;
